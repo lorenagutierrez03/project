1ba17ea07ace0b368018a2fa89f7e02a18476eca
Lorena Gutierrez <alguitierrez@misena.edu.co>
2023-01-20 12:09:00 -0500

    Se modifica controlador factorialrecursividad

--- app/Http/Controllers/FactorialreController.php
+++ app/Http/Controllers/FactorialreController.php
@@ -7,27 +7,15 @@ use Illuminate\Http\Request;
 class FactorialreController extends Controller
 {
     public function index(Request $request){
-       $numero = $request->Numero;
-        $factorial = 1;
-       // $n= 10;
-        //$suma=0;
-       
-      //  if(isset($request->Numero)){
-           
-        
-      if($factorial == 0){
-
-              return 0;
-             }else if($factorial=1){
-               return 0;
-                       }else{
-             return $numero * $factorial($numero, -1); 
+    $numero = $request->Numero; 
+    $factorial=$numero;
+      if($factorial == 1){
+              return 1;
+             }else 
+             return $numero * $factorial($numero, -1);    
           }
-             
-                    
-        
-       
-    }
-}/return view('Factorialre', compact('factorial')); 
   
-}
+    }
+//}//return view('Factorialre', compact('factorial')); 
+  
+
