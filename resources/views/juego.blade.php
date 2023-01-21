3655eaae36842d37f9f8c9599078754315eef8e8
Lorena Gutierrez <alguitierrez@misena.edu.co>
2023-01-20 21:17:31 -0500

    Se modifica juego

---
+++ resources/views/juego.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juego Simón Dice</title>
+  
+</head>
+<style>  <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+button{
+    width:300px;
+    height:300px;
+    border-radius:20px;
+    border:none;
+}
+
+button:active{
+    box-shadow:0px 0px 20px white;
+}
+
+</style>
+<body style=" background-image: url('storage/img/videoj.webp'); text-align:center;">
+   <h1 style="font">Pulsa una tecla para comenzar</h1>
+   <button id="rojo" style="background:red;" ></button>
+   <button id="verde" style="background:green;"></button>
+<br>
+   <button id="azul" style="background:blue;"></button>
+   <button id="amarillo" style="background:yellow;"></button>  
+   
+   <script type="module" src="/"></script>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,29 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>Juego - Simón Dice..</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+      
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <body  style=" background-image: url('storage/img/vj.jpg');">
+       <div class="class" style="padding-left:550px; margin-top:150px;">
+    <div class="card" style="width: 800px; height:600px;  background:#000000; ">
+    <h5 class="card-title" style="text-align:center; font-size:40px; color:#ffffff;">Bienvenido al juego : Simón Dice...</h5>
+  <img src="{{ asset('storage/img/simon.gif')}}" class="card-img-top" alt="...">
+  <div class="card-body" style="padding-left:360px;">   
+    <a href="juego" class="btn btn-primary" style="background:#ffffff; border:none; color:#000000; font-size:28px;">Vamos!</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
+  </div>
+</div>
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
     </body>
 </html>
